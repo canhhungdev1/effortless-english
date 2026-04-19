37d853e9bb0cbb122aac854429cf8a6b4702e3ce
canhhung.nguyen1 <canhhung.nguyen1@gmail.com>
2026-04-19 21:14:44 +0700

    cleanup project

--- backend/check-prisma.ts
+++
@@ -1,6 +0,0 @@
-import { PrismaClient } from '@prisma/client';
-
-console.log('Keys of PrismaClient prototype:', Object.getOwnPropertyNames(PrismaClient.prototype));
-const prisma = new PrismaClient({} as any);
-console.log('Prisma instance created');
-console.log('Internal engine config:', (prisma as any)._engineConfig);
